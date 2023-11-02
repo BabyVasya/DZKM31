c3e3d96c8e8263e01a5010515d61c8766362a470
BabyVasya <138396764+BabyVasya@users.noreply.github.com>
2023-11-02 10:12:05 +0300

    Сокращено ещё сильнее.

--- src/main/java/org/example/CalcMyFunction.java
+++ src/main/java/org/example/CalcMyFunction.java
@@ -14,33 +14,29 @@ public class CalcMyFunction extends Behaviour {
         ACLMessage receiveRequest1 = getAgent().receive(mt);
         if (receiveRequest1!=null) {
             System.out.println("Полученный запрос на расчет функции агента " + myAgent.getLocalName()+ " от агента " + receiveRequest1.getSender().getLocalName() +" выглядит так: " + receiveRequest1);
-            double d = Double.parseDouble(receiveRequest1.getContent().split(" ")[0]);
-            double x = Double.parseDouble(receiveRequest1.getContent().split(" ")[1]);
-            ACLMessage responseMsg1 = new ACLMessage(ACLMessage.CONFIRM);
-            ACLMessage responseMsg2 = new ACLMessage(ACLMessage.CONFIRM);
-            ACLMessage responseMsg3 = new ACLMessage(ACLMessage.CONFIRM);
-            responseMsg1.setContent(Functions.agent1Function(x) + " " + Functions.agent1Function(x+d) + " " + Functions.agent1Function(x-d));
-            responseMsg2.setContent(Functions.agent2Function(x) + " " + Functions.agent2Function(x+d) + " " + Functions.agent2Function(x-d));
-            responseMsg3.setContent(Functions.agent3Function(x) + " " + Functions.agent3Function(x+d) + " " + Functions.agent3Function(x-d));
-            calcForAgent("Agent1", receiveRequest1, responseMsg1, responseMsg2, responseMsg3);
-            calcForAgent("Agent2", receiveRequest1, responseMsg1, responseMsg2, responseMsg3);
-            calcForAgent("Agent3", receiveRequest1, responseMsg1, responseMsg2, responseMsg3);
+            calcForAgent("Agent1", receiveRequest1, Double.parseDouble(receiveRequest1.getContent().split(" ")[1]), Double.parseDouble(receiveRequest1.getContent().split(" ")[0]));
+            calcForAgent("Agent2", receiveRequest1, Double.parseDouble(receiveRequest1.getContent().split(" ")[1]), Double.parseDouble(receiveRequest1.getContent().split(" ")[0]));
+            calcForAgent("Agent3", receiveRequest1,  Double.parseDouble(receiveRequest1.getContent().split(" ")[1]), Double.parseDouble(receiveRequest1.getContent().split(" ")[0]));
         }
     }
 
-    private void calcForAgent (String senderName, ACLMessage receiveRequest1, ACLMessage responseMsg1, ACLMessage responseMsg2, ACLMessage responseMsg3) {
+    private void calcForAgent (String senderName, ACLMessage receiveRequest1, double x, double d) {
         if(receiveRequest1.getSender().getLocalName().equals(senderName)) {
+            ACLMessage responseMsg1 = new ACLMessage(ACLMessage.CONFIRM);
             if (myAgent.getLocalName().equals("Agent1")) {
+                responseMsg1.setContent(Functions.agent1Function(x) + " " + Functions.agent1Function(x+d) + " " + Functions.agent1Function(x-d));
                 responseMsg1.addReceiver(new AID(senderName, false));
                 getAgent().send(responseMsg1);
             }
             if (myAgent.getLocalName().equals("Agent2")) {
-                responseMsg2.addReceiver(new AID(senderName, false));
-                getAgent().send(responseMsg2);
+                responseMsg1.setContent(Functions.agent2Function(x) + " " + Functions.agent2Function(x+d) + " " + Functions.agent2Function(x-d));
+                responseMsg1.addReceiver(new AID(senderName, false));
+                getAgent().send(responseMsg1);
             }
             if (myAgent.getLocalName().equals("Agent3")) {
-                responseMsg3.addReceiver(new AID(senderName, false));
-                getAgent().send(responseMsg3);
+                responseMsg1.setContent(Functions.agent3Function(x) + " " + Functions.agent3Function(x+d) + " " + Functions.agent3Function(x-d));
+                responseMsg1.addReceiver(new AID(senderName, false));
+                getAgent().send(responseMsg1);
             }
         }
     }
